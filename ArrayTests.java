841ea2f0ff80e7691bd833883d5a252e022522bd
Anay2801 <anaymulay28@gmail.com>
2023-04-20 09:08:09 -0700

    ArrayTests.java 2nd modification

--- ArrayTests.java
+++ ArrayTests.java
@@ -3,7 +3,7 @@ import org.junit.*;
 
 public class ArrayTests {
 	@Test 
-	public void testReverseInPlace() {
+	public void testReverseInPlace1() {
     int[] input1 = { 3 };
     ArrayExamples.reverseInPlace(input1);
     assertArrayEquals(new int[]{ 3 }, input1);
@@ -11,21 +11,41 @@ public class ArrayTests {
 
 
   @Test
-  public void testReversed() {
+  public void testReversed1() {
     int[] input2 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input2));
   }
 
 @Test
-  public void testReversedError() {
+  public void testReversedError2() {
     int[] input3 = {1,2,3,4,5};
     assertArrayEquals(new int[]{5,4,3,2,1}, ArrayExamples.reversed(input3));
   }
 
 
 @Test
-  public void testReverseInPlaceError() {
+  public void testReversed2() {
     int[] input4 = {1,2,3};
     assertArrayEquals(new int[]{3,2,1}, ArrayExamples.reversed(input4));
+  }
+
+
+@Test
+  public void testAverageWithoutLowest1() {
+    double[] input5 = {1.0,2.0,3.0};
+    assertEquals(2.5, ArrayExamples.averageWithoutLowest(input5));
+  }
+
+@Test
+  public void testAverageWithoutLowest2() {
+    double[] input6 = {1.0};
+    assertEquals(0, ArrayExamples.averageWithoutLowest(input6));
+  }
+
+
+@Test
+  public void testAverageWithoutLowest3() {
+    double[] input7 = {};
+    assertEquals(0, ArrayExamples.averageWithoutLowest(input7));
   }
 }
