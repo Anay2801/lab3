47ab34852396e30e7453605c7cb6be08b85a8dcd
Anay2801 <anaymulay28@gmail.com>
2023-04-20 08:55:34 -0700

    ArrayTests.java first modification

--- ArrayTests.java
+++ ArrayTests.java
@@ -12,7 +12,20 @@ public class ArrayTests {
 
   @Test
   public void testReversed() {
-    int[] input1 = { };
-    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    int[] input2 = { };
+    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input2));
+  }
+
+@Test
+  public void testReversedError() {
+    int[] input3 = {1,2,3,4,5};
+    assertArrayEquals(new int[]{5,4,3,2,1}, ArrayExamples.reversed(input3));
+  }
+
+
+@Test
+  public void testReverseInPlaceError() {
+    int[] input4 = {1,2,3};
+    assertArrayEquals(new int[]{3,2,1}, ArrayExamples.reversed(input4));
   }
 }
