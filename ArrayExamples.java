85166f5b9ecf19f938e46807b42cbdbba32ab159
Anay2801 <anaymulay28@gmail.com>
2023-04-27 08:24:05 -0700

    Update ArrayExamples.java

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -12,9 +12,12 @@ public class ArrayExamples {
   // Returns a *new* array with all the elements of the input array in reversed
   // order
   static int[] reversed(int[] arr) {
-    int[] newArray = new int[arr.length];
+    int[] newArray = new int[arr.length]
+    int hold = 
     for(int i = 0; i < arr.length; i += 1) {
-      arr[i] = newArray[arr.length - i - 1];
+      hold = arr[i];
+       = newArray[arr.length - i - 1];
+      newArray[arr.length - i - 1] = arr[i]
     }
     return arr;
   }
